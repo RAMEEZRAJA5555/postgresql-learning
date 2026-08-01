529625a904708a15bc96158f82e421720c0063dc
rameezraja <rameezrajaoficial5@gmail.com>
2026-08-01 13:35:38 +0500

    create postgresql with sql basic file for understanding all n single file

---
+++ PostgreSQL_Single_File_CRUD/postgresql_sql_crud.sql
@@ -0,0 +1,26 @@
+-- CREATE DATABASE student_crud_db;
+
+CREATE TABLE studentcrud
+(
+    student_id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    roll_number VARCHAR(20),
+    email VARCHAR(100),
+    department VARCHAR(100),
+    semester INTEGER,
+    cgpa DECIMAL(3,2)
+);
+
+INSERT INTO studentcrud
+(name,roll_number,email,department,semester,cgpa)
+VALUES
+('Ali','101A','ali@gmail.com','Software Engineering',4,3.60);
+
+SELECT * FROM studentcrud;
+
+UPDATE studentcrud
+SET cgpa=3.90
+WHERE student_id=1;
+
+DELETE FROM studentcrud
+WHERE student_id=1;
